f58a053ac690aafcdf71c5ee9755042b455dcb54
pangdogs <pangdogs@gmail.com>
2023-04-21 20:53:18 +0800

    更新zaplog

--- logger/zap/logger.go
+++ logger/zap/logger.go
@@ -60,12 +60,12 @@ func (l *_ZapLogger) InitRuntime(ctx runtime.Context) {
 		l.sugaredLoggers[i] = l.options.ZapLogger.WithOptions(options...).Sugar()
 	}
 
-	logger.Infof(ctx, "init service plugin %s with %s", definePlugin.Name, reflect.TypeOf(_ZapLogger{}))
+	logger.Infof(ctx, "init runtime plugin %s with %s", definePlugin.Name, reflect.TypeOf(_ZapLogger{}))
 }
 
 // ShutRuntime 关闭运行时插件
 func (l *_ZapLogger) ShutRuntime(ctx runtime.Context) {
-	logger.Infof(ctx, "shut service plugin %s", definePlugin.Name)
+	logger.Infof(ctx, "shut runtime plugin %s", definePlugin.Name)
 }
 
 // Log writes a log entry, spaces are added between operands when neither is a string and a newline is appended
