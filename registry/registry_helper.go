fdb8e009e4c1f32cde17b09a07232b3b6d4fa3a0
pangdogs <pangdogs@gmail.com>
2023-05-10 17:57:31 +0800

    更新代码

--- registry/registry_helper.go
+++ registry/registry_helper.go
@@ -1,31 +1,32 @@
 package registry
 
 import (
+	"context"
 	"kit.golaxy.org/golaxy/service"
 	"time"
 )
 
 // Register 注册服务
-func Register(serviceCtx service.Context, service Service, ttl time.Duration) error {
-	return Get(serviceCtx).Register(serviceCtx, service, ttl)
+func Register(serviceCtx service.Context, ctx context.Context, service Service, ttl time.Duration) error {
+	return Get(serviceCtx).Register(ctx, service, ttl)
 }
 
 // Deregister 取消注册服务
-func Deregister(serviceCtx service.Context, service Service) error {
-	return Get(serviceCtx).Deregister(serviceCtx, service)
+func Deregister(serviceCtx service.Context, ctx context.Context, service Service) error {
+	return Get(serviceCtx).Deregister(ctx, service)
 }
 
 // GetService 查询服务
-func GetService(serviceCtx service.Context, serviceName string) ([]Service, error) {
-	return Get(serviceCtx).GetService(serviceCtx, serviceName)
+func GetService(serviceCtx service.Context, ctx context.Context, serviceName string) ([]Service, error) {
+	return Get(serviceCtx).GetService(ctx, serviceName)
 }
 
 // ListServices 查询所有服务
-func ListServices(serviceCtx service.Context) ([]Service, error) {
-	return Get(serviceCtx).ListServices(serviceCtx)
+func ListServices(serviceCtx service.Context, ctx context.Context) ([]Service, error) {
+	return Get(serviceCtx).ListServices(ctx)
 }
 
 // Watch 获取服务监听器
-func Watch(serviceCtx service.Context, serviceName string) (Watcher, error) {
-	return Get(serviceCtx).Watch(serviceCtx, serviceName)
+func Watch(serviceCtx service.Context, ctx context.Context, serviceName string) (Watcher, error) {
+	return Get(serviceCtx).Watch(ctx, serviceName)
 }
