eca96b3cc77051a1f1946a79a7d0d2d4cfe82828
pangdogs <pangdogs@gmail.com>
2023-05-17 15:46:11 +0800

    更新分布式同步插件

---
+++ dsync/dsync_helper.go
@@ -0,0 +1,8 @@
+package dsync
+
+import "kit.golaxy.org/golaxy/service"
+
+// NewDMutex returns a new distributed mutex with given name.
+func NewDMutex(serviceCtx service.Context, name string, options ...Option) DMutex {
+	return Get(serviceCtx).NewDMutex(name, options...)
+}
