a993735d8e6feb32298f551043e7ac718f19c46a
pangdogs <pangdogs@gmail.com>
2023-06-02 11:26:44 +0800

    提交协议

---
+++ transport/common.go
@@ -0,0 +1,62 @@
+package transport
+
+// Version 协议版本
+type Version = uint16
+
+const (
+	Version_V1_0 Version = 0x0100 // 协议v1.0版本
+)
+
+// SecretKeyExchangeMethod 秘钥交换函数
+type SecretKeyExchangeMethod = uint8
+
+const (
+	SecretKeyExchangeMethod_None  SecretKeyExchangeMethod = iota // 未设置
+	SecretKeyExchangeMethod_ECDHE                                // ECDHE算法
+)
+
+// AsymmetricEncryptMethod 非对称加密函数
+type AsymmetricEncryptMethod = uint8
+
+const (
+	AsymmetricEncryptMethod_None AsymmetricEncryptMethod = iota // 未设置
+	AsymmetricEncryptMethod_RSA                                 // RSA算法
+	AsymmetricEncryptMethod_ECC                                 // ECC算法
+)
+
+// SymmetricEncryptMethod 对称加密函数
+type SymmetricEncryptMethod = uint8
+
+const (
+	SymmetricEncryptMethod_None     SymmetricEncryptMethod = iota // 未设置
+	SymmetricEncryptMethod_AES256                                 // AES256算法
+	SymmetricEncryptMethod_ChaCha20                               // ChaCha20算法
+)
+
+// BlockCipherMode 对称加密算法分组模式
+type BlockCipherMode = uint8
+
+const (
+	BlockCipherMode_None BlockCipherMode = iota // 未设置
+	BlockCipherMode_ECB                         // ECB模式
+	BlockCipherMode_GCM                         // GCM模式
+)
+
+// HashMethod 摘要函数
+type HashMethod = uint8
+
+const (
+	HashMethod_Mask   HashMethod = iota // 未设置
+	HashMethod_Fnv1a                    // Fnv-1a算法
+	HashMethod_SHA256                   // SHA256算法
+)
+
+// CompressionMethod 压缩函数
+type CompressionMethod = uint8
+
+const (
+	CompressionMethod_Gzip   CompressionMethod = iota // Gzip压缩算法
+	CompressionMethod_Brotli                          // Brotli压缩算法
+	CompressionMethod_LZ4                             // LZ4压缩算法
+	CompressionMethod_Snappy                          // Snappy压缩算法
+)
