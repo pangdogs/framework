7f75930f867b157c14884959c12908899c8eda3d
pangdogs <pangdogs@gmail.com>
2023-06-18 01:29:02 +0800

    更新协议

--- transport/msg_changecipherspec.go
+++ transport/msg_changecipherspec.go
@@ -2,6 +2,11 @@ package transport
 
 import "kit.golaxy.org/plugins/transport/binaryutil"
 
+// ChangeCipherSpec消息标志位
+const (
+	Flag_VerifyEncryption Flag = 1 << (iota + Flag_Customize) // 交换秘钥后，在服务端变更密码规范消息中携带，表示需要验证加密是否成功
+)
+
 // MsgChangeCipherSpec 变更密码规范
 type MsgChangeCipherSpec struct {
 	EncryptedHello []byte // 加密Hello消息，用于双方验证加密是否成功
--- transport/protocol/control.go
+++ transport/protocol/control.go
@@ -1,1 +1,14 @@
 package tcp
+
+import (
+	"kit.golaxy.org/plugins/transport/codec"
+	"net"
+)
+
+// Ctrl 控制协议
+type Ctrl struct {
+	Conn       net.Conn       // 网络连接
+	Encoder    codec.IEncoder // 消息包编码器
+	Decoder    codec.IDecoder // 消息包解码器
+	RetryTimes int            // io超时重试次数
+}
