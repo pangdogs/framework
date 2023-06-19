5fb9341d333d3e5f9da73499b73fbda1fad49788
pangdogs <pangdogs@gmail.com>
2023-06-19 17:30:19 +0800

    更新协议

--- transport/msg_heartbeat.go
+++ transport/msg_heartbeat.go
@@ -3,15 +3,15 @@ package transport
 // MsgHeartbeat 心跳，消息体为空，可以不解析
 type MsgHeartbeat struct{}
 
-func (m *MsgHeartbeat) Read(p []byte) (int, error) {
+func (MsgHeartbeat) Read(p []byte) (int, error) {
 	return 0, nil
 }
 
-func (m *MsgHeartbeat) Write(p []byte) (int, error) {
+func (MsgHeartbeat) Write(p []byte) (int, error) {
 	return 0, nil
 }
 
-func (m *MsgHeartbeat) Size() int {
+func (MsgHeartbeat) Size() int {
 	return 0
 }
 
