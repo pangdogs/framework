4737e2dbc328c807b4b419925d338ce5f4615294
pangdogs <pangdogs@gmail.com>
2023-08-20 14:37:43 +0800

    更新代码

--- transport/protocol/transceiver_retry.go
+++ transport/protocol/transceiver_retry.go
@@ -3,7 +3,6 @@ package protocol
 import (
 	"errors"
 	"kit.golaxy.org/plugins/transport"
-	"os"
 )
 
 // Retry 网络io超时时重试
@@ -17,12 +16,12 @@ func (r Retry) Send(err error) error {
 	if err == nil {
 		return nil
 	}
-	if !errors.Is(err, os.ErrDeadlineExceeded) {
+	if !errors.Is(err, ErrTimeout) {
 		return err
 	}
 	for i := r.Times; i > 0; i-- {
 		if err = r.Transceiver.Resend(); err != nil {
-			if errors.Is(err, os.ErrDeadlineExceeded) {
+			if errors.Is(err, ErrTimeout) {
 				continue
 			}
 		}
@@ -36,13 +35,13 @@ func (r Retry) Recv(e Event[transport.Msg], err error) (Event[transport.Msg], er
 	if err == nil {
 		return e, nil
 	}
-	if !errors.Is(err, ErrDiscardSeq) && !errors.Is(err, os.ErrDeadlineExceeded) {
+	if !errors.Is(err, ErrTimeout) && !errors.Is(err, ErrDiscardSeq) {
 		return e, err
 	}
 	for i := r.Times; i > 0; {
 		e, err = r.Transceiver.Recv()
 		if err != nil {
-			if errors.Is(err, os.ErrDeadlineExceeded) {
+			if errors.Is(err, ErrTimeout) {
 				i--
 				continue
 			}
