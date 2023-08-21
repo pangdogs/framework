7955815c944529da883a6e4295d45755b81f7800
pangdogs <pangdogs@gmail.com>
2023-08-21 11:16:20 +0800

    更新代码

---
+++ gate/gate_helper.go
@@ -0,0 +1,18 @@
+package gate
+
+import "kit.golaxy.org/golaxy/service"
+
+// GetSession 查询会话
+func GetSession(serviceCtx service.Context, sessionId string) (Session, bool) {
+	return Get(serviceCtx).GetSession(sessionId)
+}
+
+// RangeSessions 遍历所有会话
+func RangeSessions(serviceCtx service.Context, fun func(session Session) bool) {
+	Get(serviceCtx).RangeSessions(fun)
+}
+
+// CountSessions 统计所有会话数量
+func CountSessions(serviceCtx service.Context) int {
+	return Get(serviceCtx).CountSessions()
+}
