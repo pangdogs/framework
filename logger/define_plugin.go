b79b21d3c8a28983881ac0a3a25eddd24b517d95
pangdogs <pangdogs@gmail.com>
2023-08-23 00:23:04 +0800

    更新代码

--- logger/define_plugin.go
+++ logger/define_plugin.go
@@ -6,8 +6,8 @@ var (
 	definePlugin = define.DefinePluginInterface[Logger]()
 	// Name 插件名称
 	Name = definePlugin.Name
-	// Get 获取插件
-	Get = definePlugin.Get
-	// TryGet 尝试获取插件
-	TryGet = definePlugin.TryGet
+	// Fetch 获取插件
+	Fetch = definePlugin.Fetch
+	// Access 访问插件
+	Access = definePlugin.Access
 )
