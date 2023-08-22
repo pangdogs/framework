4bd65340b3706e8d378dc36c61300cd47502e52d
pangdogs <pangdogs@gmail.com>
2023-08-23 00:55:06 +0800

    更新代码

--- registry/registry_helper.go
+++ registry/registry_helper.go
@@ -8,25 +8,25 @@ import (
 
 // Register 注册服务
 func Register(serviceCtx service.Context, ctx context.Context, service Service, ttl time.Duration) error {
-	return Get(serviceCtx).Register(ctx, service, ttl)
+	return Fetch(serviceCtx).Register(ctx, service, ttl)
 }
 
 // Deregister 取消注册服务
 func Deregister(serviceCtx service.Context, ctx context.Context, service Service) error {
-	return Get(serviceCtx).Deregister(ctx, service)
+	return Fetch(serviceCtx).Deregister(ctx, service)
 }
 
 // GetService 查询服务
 func GetService(serviceCtx service.Context, ctx context.Context, serviceName string) ([]Service, error) {
-	return Get(serviceCtx).GetService(ctx, serviceName)
+	return Fetch(serviceCtx).GetService(ctx, serviceName)
 }
 
 // ListServices 查询所有服务
 func ListServices(serviceCtx service.Context, ctx context.Context) ([]Service, error) {
-	return Get(serviceCtx).ListServices(ctx)
+	return Fetch(serviceCtx).ListServices(ctx)
 }
 
 // Watch 获取服务监听器
 func Watch(serviceCtx service.Context, ctx context.Context, serviceName string) (Watcher, error) {
-	return Get(serviceCtx).Watch(ctx, serviceName)
+	return Fetch(serviceCtx).Watch(ctx, serviceName)
 }
