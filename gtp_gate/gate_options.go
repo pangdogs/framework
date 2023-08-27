53124d7b4edd297607418fec892b320cb06690a7
pangdogs <pangdogs@gmail.com>
2023-08-27 18:28:09 +0800

    更新代码

--- gtp_gate/gate_options.go
+++ gtp_gate/gate_options.go
@@ -6,6 +6,7 @@ import (
 	"kit.golaxy.org/golaxy/service"
 	"kit.golaxy.org/plugins/gtp"
 	"kit.golaxy.org/plugins/gtp/codec"
+	"kit.golaxy.org/plugins/gtp/transport"
 	"math/big"
 	"net"
 	"time"
@@ -14,41 +15,44 @@ import (
 type _GateOption struct{}
 
 type (
-	ClientAuthHandler = func(ctx service.Context, conn net.Conn, token string, extensions []byte) error // 客户端鉴权鉴权处理器
+	ClientAuthHandler          = func(ctx service.Context, conn net.Conn, token string, extensions []byte) error // 客户端鉴权鉴权处理器
+	SessionStateChangedHandler = func(session Session, old, new SessionState)                                    // 会话状态变化的处理器
+	SessionRecvDataHandler     = func(session Session, data []byte) error                                        // 会话接收的数据的处理器
+	SessionRecvEventHandler    = func(session Session, event transport.Event[gtp.Msg]) error                     // 会话接收的自定义事件的处理器
 )
 
 type GateOptions struct {
-	Endpoints                      []string               // 所有监听地址
-	TLSConfig                      *tls.Config            // TLS配置，nil表示不使用TLS加密链路
-	TCPNoDelay                     *bool                  // TCP的NoDelay选项，nil表示使用系统默认值
-	TCPQuickAck                    *bool                  // TCP的QuickAck选项，nil表示使用系统默认值
-	TCPRecvBuf                     *int                   // TCP的RecvBuf大小（字节）选项，nil表示使用系统默认值
-	TCPSendBuf                     *int                   // TCP的SendBuf大小（字节）选项，nil表示使用系统默认值
-	TCPLinger                      *int                   // TCP的PLinger选项，nil表示使用系统默认值
-	IOTimeout                      time.Duration          // 网络io超时时间
-	IORetryTimes                   int                    // 网络io超时后的重试次数
-	IOBufferCap                    int                    // 网络io缓存容量（字节）
-	DecoderMsgCreator              codec.IMsgCreator      // 消息包解码器的消息构建器
-	AgreeClientEncryptionProposal  bool                   // 是否同意使用客户端建议的加密方案
-	EncCipherSuite                 gtp.CipherSuite        // 加密通信中的密码学套件
-	EncNonceStep                   *big.Int               // 加密通信中，使用需要nonce的加密算法时，每次加解密自增值
-	EncECDHENamedCurve             gtp.NamedCurve         // 加密通信中，在ECDHE交换秘钥时使用的曲线类型
-	EncSignatureAlgorithm          gtp.SignatureAlgorithm // 加密通信中的签名算法
-	EncSignaturePrivateKey         crypto.PrivateKey      // 加密通信中，签名用的私钥
-	EncVerifyClientSignature       bool                   // 加密通信中，是否验证客户端签名
-	EncVerifySignaturePublicKey    crypto.PublicKey       // 加密通信中，验证客户端签名用的公钥
-	AgreeClientCompressionProposal bool                   // 是否同意使用客户端建议的压缩方案
-	Compression                    gtp.Compression        // 通信中的压缩函数
-	CompressedSize                 int                    // 通信中启用压缩阀值（字节），<=0表示不开启
-	ClientAuthHandlers             []ClientAuthHandler    // 客户端鉴权鉴权处理器列表
-	SessionInactiveTimeout         time.Duration          // 会话不活跃后的超时时间
-	SessionStateChangedHandlers    []StateChangedHandler  // 会话状态变化的处理器列表（优先级高于会话的处理器）
-	SessionSendDataChanSize        int                    // 会话发送数据的channel的大小，<=0表示不使用channel
-	SessionRecvDataChanSize        int                    // 会话接收数据的channel的大小，<=0表示不使用channel
-	SessionSendEventSize           int                    // 会话发送自定义事件的channel的大小，<=0表示不使用channel
-	SessionRecvEventSize           int                    // 会话接收自定义事件的channel的大小，<=0表示不使用channel
-	SessionRecvDataHandlers        []RecvDataHandler      // 会话接收的数据的处理器列表（优先级高于会话的处理器）
-	SessionRecvEventHandlers       []RecvEventHandler     // 会话接收的自定义事件的处理器列表（优先级高于会话的处理器）
+	Endpoints                      []string                     // 所有监听地址
+	TLSConfig                      *tls.Config                  // TLS配置，nil表示不使用TLS加密链路
+	TCPNoDelay                     *bool                        // TCP的NoDelay选项，nil表示使用系统默认值
+	TCPQuickAck                    *bool                        // TCP的QuickAck选项，nil表示使用系统默认值
+	TCPRecvBuf                     *int                         // TCP的RecvBuf大小（字节）选项，nil表示使用系统默认值
+	TCPSendBuf                     *int                         // TCP的SendBuf大小（字节）选项，nil表示使用系统默认值
+	TCPLinger                      *int                         // TCP的PLinger选项，nil表示使用系统默认值
+	IOTimeout                      time.Duration                // 网络io超时时间
+	IORetryTimes                   int                          // 网络io超时后的重试次数
+	IOBufferCap                    int                          // 网络io缓存容量（字节）
+	DecoderMsgCreator              codec.IMsgCreator            // 消息包解码器的消息构建器
+	AgreeClientEncryptionProposal  bool                         // 是否同意使用客户端建议的加密方案
+	EncCipherSuite                 gtp.CipherSuite              // 加密通信中的密码学套件
+	EncNonceStep                   *big.Int                     // 加密通信中，使用需要nonce的加密算法时，每次加解密自增值
+	EncECDHENamedCurve             gtp.NamedCurve               // 加密通信中，在ECDHE交换秘钥时使用的曲线类型
+	EncSignatureAlgorithm          gtp.SignatureAlgorithm       // 加密通信中的签名算法
+	EncSignaturePrivateKey         crypto.PrivateKey            // 加密通信中，签名用的私钥
+	EncVerifyClientSignature       bool                         // 加密通信中，是否验证客户端签名
+	EncVerifySignaturePublicKey    crypto.PublicKey             // 加密通信中，验证客户端签名用的公钥
+	AgreeClientCompressionProposal bool                         // 是否同意使用客户端建议的压缩方案
+	Compression                    gtp.Compression              // 通信中的压缩函数
+	CompressedSize                 int                          // 通信中启用压缩阀值（字节），<=0表示不开启
+	ClientAuthHandlers             []ClientAuthHandler          // 客户端鉴权鉴权处理器列表
+	SessionInactiveTimeout         time.Duration                // 会话不活跃后的超时时间
+	SessionStateChangedHandlers    []SessionStateChangedHandler // 会话状态变化的处理器列表（优先级高于会话的处理器）
+	SessionSendDataChanSize        int                          // 会话发送数据的channel的大小，<=0表示不使用channel
+	SessionRecvDataChanSize        int                          // 会话接收数据的channel的大小，<=0表示不使用channel
+	SessionSendEventSize           int                          // 会话发送自定义事件的channel的大小，<=0表示不使用channel
+	SessionRecvEventSize           int                          // 会话接收自定义事件的channel的大小，<=0表示不使用channel
+	SessionRecvDataHandlers        []SessionRecvDataHandler     // 会话接收的数据的处理器列表（优先级高于会话的处理器）
+	SessionRecvEventHandlers       []SessionRecvEventHandler    // 会话接收的自定义事件的处理器列表（优先级高于会话的处理器）
 }
 
 type GateOption func(options *GateOptions)
@@ -251,7 +255,7 @@ func (_GateOption) SessionInactiveTimeout(d time.Duration) GateOption {
 	}
 }
 
-func (_GateOption) SessionStateChangedHandlers(handlers ...StateChangedHandler) GateOption {
+func (_GateOption) SessionStateChangedHandlers(handlers ...SessionStateChangedHandler) GateOption {
 	return func(options *GateOptions) {
 		options.SessionStateChangedHandlers = handlers
 	}
@@ -281,13 +285,13 @@ func (_GateOption) SessionRecvEventSize(size int) GateOption {
 	}
 }
 
-func (_GateOption) SessionRecvDataHandlers(handlers ...RecvDataHandler) GateOption {
+func (_GateOption) SessionRecvDataHandlers(handlers ...SessionRecvDataHandler) GateOption {
 	return func(options *GateOptions) {
 		options.SessionRecvDataHandlers = handlers
 	}
 }
 
-func (_GateOption) SessionRecvEventHandlers(handlers ...RecvEventHandler) GateOption {
+func (_GateOption) SessionRecvEventHandlers(handlers ...SessionRecvEventHandler) GateOption {
 	return func(options *GateOptions) {
 		options.SessionRecvEventHandlers = handlers
 	}
