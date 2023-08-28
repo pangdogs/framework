b22c7ca822a0f5545ea129f7d408b16ffa2537b1
pangdogs <pangdogs@gmail.com>
2023-08-28 11:39:21 +0800

    更新代码

--- gtp_gate/define_plugin.go
+++ gtp_gate/define_plugin.go
@@ -6,6 +6,8 @@ import (
 
 var (
 	definePlugin = define.DefineServicePlugin[Gate, GateOption](newGtpGate)
+	// Name 插件名称
+	Name = definePlugin.Name
 	// Fetch 获取插件
 	Fetch = definePlugin.Fetch
 	// Access 访问插件
