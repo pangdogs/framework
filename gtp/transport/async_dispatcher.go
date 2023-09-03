2ffef3d0c1985df9c84dc311045af22b31ea5414
pangdogs <pangdogs@gmail.com>
2023-09-03 11:23:04 +0800

    更新代码

--- gtp/transport/async_dispatcher.go
+++ gtp/transport/async_dispatcher.go
@@ -3,7 +3,7 @@ package transport
 import (
 	"context"
 	"errors"
-	"kit.golaxy.org/golaxy/util/concurrent"
+	"sync"
 	"sync/atomic"
 	"time"
 )
@@ -15,25 +15,19 @@ var (
 	ErrAsyncRespIncorrectType = errors.New("async response has incorrect value type") // 异步响应的返回值类型错误
 )
 
-// IAsyncResp 异步响应接口
-type IAsyncResp interface {
-	// Push 填入返回结果
-	Push(v any, err error)
-}
-
 // Ret 返回结果
 type Ret[T any] struct {
 	Value T     // 返回值
 	Error error // 返回错误
 }
 
-func newRet[T any](v any, err error) (ret Ret[T]) {
+func newRet[T any](rv any, err error) (ret Ret[T]) {
 	if err != nil {
 		ret.Error = err
 	} else {
-		rv, ok := v.(T)
+		value, ok := rv.(T)
 		if ok {
-			ret.Value = rv
+			ret.Value = value
 		} else {
 			ret.Error = ErrAsyncRespIncorrectType
 		}
@@ -41,12 +35,18 @@ func newRet[T any](v any, err error) (ret Ret[T]) {
 	return
 }
 
+// IAsyncResp 异步响应接口
+type IAsyncResp interface {
+	// Push 填入返回结果
+	Push(v any, err error)
+}
+
 // AsyncRespChan 异步响应接收返回值的channel
 type AsyncRespChan[T any] chan Ret[T]
 
 // Push 填入返回结果
-func (resp AsyncRespChan[T]) Push(v any, err error) {
-	resp <- newRet[T](v, err)
+func (resp AsyncRespChan[T]) Push(rv any, err error) {
+	resp <- newRet[T](rv, err)
 	close(resp)
 }
 
@@ -54,20 +54,51 @@ func (resp AsyncRespChan[T]) Push(v any, err error) {
 type AsyncRespHandler[T any] func(ret Ret[T])
 
 // Push 填入返回结果
-func (resp AsyncRespHandler[T]) Push(v any, err error) {
-	(resp)(newRet[T](v, err))
+func (resp AsyncRespHandler[T]) Push(rv any, err error) {
+	(resp)(newRet[T](rv, err))
 }
 
 type _AsyncReq struct {
+	Ctx    context.Context
+	Cancel context.CancelFunc
+	Id     int64
 	Resp   IAsyncResp
-	Finish context.CancelFunc
+}
+
+func (req *_AsyncReq) Wait(d *AsyncDispatcher, ctx context.Context) {
+	timer := time.NewTimer(d.Timeout)
+	defer timer.Stop()
+
+	select {
+	case <-ctx.Done():
+		d.Dispatching(req.Id, nil, ErrAsyncReqCancelled)
+	case <-timer.C:
+		d.Dispatching(req.Id, nil, ErrAsyncReqTimeout)
+	case <-req.Ctx.Done():
+		return
+	}
+}
+
+func (req *_AsyncReq) Reply(rv any, err error) {
+	req.Cancel()
+	req.Resp.Push(rv, err)
+}
+
+func newAsyncReq(reqId int64, resp IAsyncResp) *_AsyncReq {
+	ctx, cancel := context.WithCancel(context.Background())
+	return &_AsyncReq{
+		Ctx:    ctx,
+		Cancel: cancel,
+		Id:     reqId,
+		Resp:   resp,
+	}
 }
 
 // AsyncDispatcher 异步请求响应分发器
 type AsyncDispatcher struct {
-	ReqId    int64                            // 请求id生成器
-	Timeout  time.Duration                    // 请求超时时间
-	waitResp concurrent.Map[int64, _AsyncReq] // 等待异步响应
+	ReqId    int64         // 请求id生成器
+	Timeout  time.Duration // 请求超时时间
+	waitResp sync.Map      // 等待异步响应
 }
 
 // MakeRequest 创建异步请求
@@ -80,35 +111,20 @@ func (d *AsyncDispatcher) MakeRequest(ctx context.Context, resp IAsyncResp) (int
 		ctx = context.Background()
 	}
 
-	fin, cancel := context.WithCancel(context.Background())
+	req := newAsyncReq(atomic.AddInt64(&d.ReqId, 1), resp)
+	d.waitResp.Store(req.Id, req)
 
-	reqId := atomic.AddInt64(&d.ReqId, 1)
-	d.waitResp.Store(reqId, _AsyncReq{Resp: resp, Finish: cancel})
+	go req.Wait(d, ctx)
 
-	go func() {
-		timer := time.NewTimer(d.Timeout)
-		defer timer.Stop()
-
-		select {
-		case <-ctx.Done():
-			d.Dispatching(reqId, nil, ErrAsyncReqCancelled)
-		case <-timer.C:
-			d.Dispatching(reqId, nil, ErrAsyncReqTimeout)
-		case <-fin.Done():
-			return
-		}
-	}()
-
-	return reqId, nil
+	return req.Id, nil
 }
 
 // Dispatching 分发异步响应返回值
-func (d *AsyncDispatcher) Dispatching(reqId int64, v any, err error) error {
-	req, ok := d.waitResp.LoadAndDelete(reqId)
+func (d *AsyncDispatcher) Dispatching(reqId int64, rv any, err error) error {
+	v, ok := d.waitResp.LoadAndDelete(reqId)
 	if !ok {
 		return ErrAsyncReqNotFound
 	}
-	req.Resp.Push(v, err)
-	req.Finish()
+	v.(*_AsyncReq).Reply(rv, err)
 	return nil
 }
