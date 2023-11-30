e69936379a1f65598e9e4a3fa2b1564786b583fe
pangdogs <pangdogs@gmail.com>
2023-11-30 15:20:18 +0800

    修改gtp协议

--- gtp/transport/transceiver_sequencedbuffer.go
+++ gtp/transport/transceiver_sequencedbuffer.go
@@ -5,33 +5,9 @@ import (
 	"io"
 	"kit.golaxy.org/golaxy"
 	"kit.golaxy.org/plugins/gtp"
-	"kit.golaxy.org/plugins/gtp/codec"
 	"kit.golaxy.org/plugins/util/binaryutil"
 	"sync/atomic"
 )
-
-// Buffer 缓存
-type Buffer interface {
-	io.Writer
-	io.WriterTo
-	codec.IValidate
-	// Synchronization 同步对端时序，对齐缓存序号
-	Synchronization(remoteRecvSeq uint32) error
-	// Ack 确认消息序号
-	Ack(ack uint32) error
-	// SendSeq 发送消息序号
-	SendSeq() uint32
-	// RecvSeq 接收消息序号
-	RecvSeq() uint32
-	// AckSeq 当前ack序号
-	AckSeq() uint32
-	// Cap 缓存区容量
-	Cap() int
-	// Cached 已缓存大小
-	Cached() int
-	// Clean 清理
-	Clean()
-}
 
 // _SequencedFrame 时序帧
 type _SequencedFrame struct {
