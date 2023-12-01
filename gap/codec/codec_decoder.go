a43de4d0949bfbafcbda2da78704c9cb7c583e70
pangdogs <pangdogs@gmail.com>
2023-12-01 15:33:07 +0800

    更新gap协议

---
+++ gap/codec/codec_decoder.go
@@ -0,0 +1,102 @@
+package codec
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"kit.golaxy.org/golaxy"
+	"kit.golaxy.org/plugins/gap"
+)
+
+var (
+	ErrDataNotEnough = errors.New("data not enough") // 数据不足
+)
+
+// DefaultDecoder 默认消息包解码器
+func DefaultDecoder() Decoder {
+	return Decoder{MsgCreator: gap.DefaultMsgCreator()}
+}
+
+// Decoder 消息包解码器
+type Decoder struct {
+	MsgCreator gap.IMsgCreator // 消息对象构建器
+	buffer     bytes.Buffer    // buffer
+}
+
+// Write implements io.Writer
+func (d *Decoder) Write(p []byte) (int, error) {
+	return d.buffer.Write(p)
+}
+
+// ReadFrom implements io.ReaderFrom
+func (d *Decoder) ReadFrom(r io.Reader) (int64, error) {
+	if r == nil {
+		return 0, fmt.Errorf("%w: r is nil", golaxy.ErrArgs)
+	}
+
+	var buff [bytes.MinRead]byte
+
+	n, err := r.Read(buff[:])
+	if n > 0 {
+		d.buffer.Write(buff[:n])
+	}
+
+	return int64(n), err
+}
+
+// Reset 重置缓存
+func (d *Decoder) Reset() {
+	d.buffer.Reset()
+}
+
+// Decode 从缓存，解码消息包
+func (d *Decoder) Decode() (gap.MsgPacket, error) {
+	return d.DecodeBuff(&d.buffer)
+}
+
+// DecodeBuff 从指定buff，解码消息包
+func (d Decoder) DecodeBuff(buff *bytes.Buffer) (gap.MsgPacket, error) {
+	mp, n, err := d.decode(buff.Bytes())
+	buff.Next(n)
+	return mp, err
+}
+
+// DecodeBytes 从指定bytes，解码消息包
+func (d Decoder) DecodeBytes(data []byte) (gap.MsgPacket, error) {
+	mp, _, err := d.decode(data)
+	return mp, err
+}
+
+// decode 解码消息包
+func (d Decoder) decode(data []byte) (gap.MsgPacket, int, error) {
+	if d.MsgCreator == nil {
+		return gap.MsgPacket{}, 0, errors.New("setting MsgCreator is nil")
+	}
+
+	mp := gap.MsgPacket{}
+
+	// 读取消息头
+	n, err := mp.Write(data)
+	if err != nil {
+		return gap.MsgPacket{}, 0, fmt.Errorf("read msg-packet-head failed, %w", err)
+	}
+
+	if len(data) < int(mp.Head.Len) {
+		return gap.MsgPacket{}, 0, fmt.Errorf("%w (%d < %d)", ErrDataNotEnough, len(data), mp.Head.Len)
+	}
+
+	// 创建消息体
+	mp.Msg, err = d.MsgCreator.Spawn(mp.Head.MsgId)
+	if err != nil {
+		return gap.MsgPacket{}, int(mp.Head.Len), fmt.Errorf("spawn msg failed, %w (%d)", err, mp.Head.MsgId)
+	}
+
+	// 读取消息
+	_, err = mp.Msg.Write(data[n:])
+	if err != nil {
+		return gap.MsgPacket{}, int(mp.Head.Len), fmt.Errorf("read msg failed, %w", err)
+	}
+
+	return mp, int(mp.Head.Len), nil
+}
---
+++ gap/codec/codec_encoder.go
@@ -0,0 +1,105 @@
+package codec
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"kit.golaxy.org/golaxy"
+	"kit.golaxy.org/plugins/gap"
+	"kit.golaxy.org/plugins/util/binaryutil"
+)
+
+// DefaultEncoder 默认消息包编码器
+func DefaultEncoder() Encoder {
+	return Encoder{}
+}
+
+// Encoder 消息包编码器
+type Encoder struct {
+	buffer bytes.Buffer // buffer
+}
+
+// Read implements io.Reader
+func (e *Encoder) Read(p []byte) (int, error) {
+	return e.buffer.Read(p)
+}
+
+// WriteTo implements io.WriterTo
+func (e *Encoder) WriteTo(w io.Writer) (int64, error) {
+	if w == nil {
+		return 0, fmt.Errorf("%w: w is nil", golaxy.ErrArgs)
+	}
+	return e.buffer.WriteTo(w)
+}
+
+// Reset 重置缓存
+func (e *Encoder) Reset() {
+	e.buffer.Reset()
+}
+
+// Encode 编码消息包，写入缓存
+func (e *Encoder) Encode(seq int64, msg gap.Msg) error {
+	return e.EncodeWriter(&e.buffer, seq, msg)
+}
+
+// EncodeWriter 编码消息包，写入指定writer
+func (e Encoder) EncodeWriter(writer io.Writer, seq int64, msg gap.Msg) error {
+	if writer == nil {
+		return fmt.Errorf("%w: writer is nil", golaxy.ErrArgs)
+	}
+
+	mpBuf, err := e.encode(seq, msg)
+	if err != nil {
+		return err
+	}
+	defer mpBuf.Release()
+
+	_, err = writer.Write(mpBuf.Data())
+	if err != nil {
+		return fmt.Errorf("write msg-packet-bytes failed, %w", err)
+	}
+
+	return nil
+}
+
+// EncodeBuff 编码消息包，写入指定buffer
+func (e Encoder) EncodeBuff(buff *bytes.Buffer, seq int64, msg gap.Msg) error {
+	if buff == nil {
+		return fmt.Errorf("%w: buff is nil", golaxy.ErrArgs)
+	}
+	return e.EncodeWriter(buff, seq, msg)
+}
+
+// EncodeBytes 编码消息包，返回可回收bytes
+func (e Encoder) EncodeBytes(seq int64, msg gap.Msg) (binaryutil.RecycleBytes, error) {
+	return e.encode(seq, msg)
+}
+
+// encode 编码消息包
+func (Encoder) encode(seq int64, msg gap.Msg) (ret binaryutil.RecycleBytes, err error) {
+	if msg == nil {
+		return binaryutil.MakeNonRecycleBytes(nil), fmt.Errorf("%w: msg is nil", golaxy.ErrArgs)
+	}
+
+	mp := gap.MsgPacket{
+		Head: gap.MsgHead{
+			MsgId: msg.MsgId(),
+			Seq:   seq,
+		},
+		Msg: msg,
+	}
+	mp.Head.Len = uint32(mp.Size())
+
+	mpBuf := binaryutil.MakeRecycleBytes(binaryutil.BytesPool.Get(int(mp.Head.Len)))
+	defer func() {
+		if err != nil {
+			mpBuf.Release()
+		}
+	}()
+
+	if _, err := mp.Read(mpBuf.Data()); err != nil {
+		return binaryutil.MakeNonRecycleBytes(nil), err
+	}
+
+	return mpBuf, nil
+}
