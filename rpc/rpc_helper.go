224b90db7b23b7ecb424b5e7ba2db6cbfbf771ee
pangdogs <pangdogs@gmail.com>
2024-01-07 12:33:24 +0800

    更新代码

--- rpc/rpc_helper.go
+++ rpc/rpc_helper.go
@@ -3,17 +3,11 @@ package rpc
 import (
 	"kit.golaxy.org/golaxy/runtime"
 	"kit.golaxy.org/golaxy/service"
-	"kit.golaxy.org/plugins/gap/variant"
 )
 
 // RPC RPC调用
 func RPC(servCtx service.Context, dst, path string, args ...any) runtime.AsyncRet {
 	return Using(servCtx).RPC(dst, path, args...)
-}
-
-func Return[T any](ret runtime.Ret) error {
-	*ret.Value.([]any)[0].(*variant.Int)
-
 }
 
 // OneWayRPC 单向RPC调用
