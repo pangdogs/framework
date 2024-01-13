dd79e9d480afea391eef44a9c68d8604a946b014
pangdogs <63327170+pangdogs@users.noreply.github.com>
2024-01-14 01:35:32 +0800

    修改代码

--- gtp_gate/session_datawatcher.go
+++ gtp_gate/session_datawatcher.go
@@ -5,21 +5,21 @@ import (
 	"golang.org/x/net/context"
 )
 
-func (s *_Session) newEventWatcher(ctx context.Context, handler RecvEventHandler) *_EventWatcher {
+func (s *_Session) newDataWatcher(ctx context.Context, handler RecvDataHandler) *_DataWatcher {
 	if ctx == nil {
 		ctx = context.Background()
 	}
 
 	ctx, cancel := context.WithCancel(ctx)
 
-	watcher := &_EventWatcher{
+	watcher := &_DataWatcher{
 		Context:     ctx,
 		cancel:      cancel,
 		stoppedChan: make(chan struct{}),
 		session:     s,
 		handler:     handler,
 	}
-	s.eventWatchers.Append(watcher)
+	s.dataWatchers.Append(watcher)
 
 	s.gate.wg.Add(1)
 	go watcher.mainLoop()
@@ -27,20 +27,20 @@ func (s *_Session) newEventWatcher(ctx context.Context, handler RecvEventHandler
 	return watcher
 }
 
-type _EventWatcher struct {
+type _DataWatcher struct {
 	context.Context
 	cancel      context.CancelFunc
 	stoppedChan chan struct{}
 	session     *_Session
-	handler     RecvEventHandler
+	handler     RecvDataHandler
 }
 
-func (w *_EventWatcher) Stop() <-chan struct{} {
+func (w *_DataWatcher) Stop() <-chan struct{} {
 	w.cancel()
 	return w.stoppedChan
 }
 
-func (w *_EventWatcher) mainLoop() {
+func (w *_DataWatcher) mainLoop() {
 	defer func() {
 		w.cancel()
 		w.session.gate.wg.Done()
@@ -52,8 +52,8 @@ func (w *_EventWatcher) mainLoop() {
 	case <-w.session.Done():
 	}
 
-	w.session.eventWatchers.AutoLock(func(watchers *[]*_EventWatcher) {
-		*watchers = pie.DropWhile(*watchers, func(other *_EventWatcher) bool {
+	w.session.dataWatchers.AutoLock(func(watchers *[]*_DataWatcher) {
+		*watchers = pie.DropWhile(*watchers, func(other *_DataWatcher) bool {
 			return other == w
 		})
 	})
