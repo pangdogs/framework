da7ee3721c6d0339956b1e283468c9d52f039d5f
pangdogs <pangdogs@gmail.com>
2024-01-13 23:14:07 +0800

    更新代码

--- gtp_cli/client_eventwatcher.go
+++ gtp_cli/client_eventwatcher.go
@@ -1,25 +1,25 @@
-package gtp_client
+package gtp_cli
 
 import (
 	"github.com/elliotchance/pie/v2"
 	"golang.org/x/net/context"
 )
 
-func (c *Client) newEventWatcher(ctx context.Context, handler RecvEventHandler) *_EventWatcher {
+func (c *Client) newDataWatcher(ctx context.Context, handler RecvDataHandler) *_DataWatcher {
 	if ctx == nil {
 		ctx = context.Background()
 	}
 
 	ctx, cancel := context.WithCancel(ctx)
 
-	watcher := &_EventWatcher{
+	watcher := &_DataWatcher{
 		Context:     ctx,
 		cancel:      cancel,
 		stoppedChan: make(chan struct{}),
 		client:      c,
 		handler:     handler,
 	}
-	c.eventWatchers.Append(watcher)
+	c.dataWatchers.Append(watcher)
 
 	c.wg.Add(1)
 	go watcher.mainLoop()
@@ -27,20 +27,20 @@ func (c *Client) newEventWatcher(ctx context.Context, handler RecvEventHandler) 
 	return watcher
 }
 
-type _EventWatcher struct {
+type _DataWatcher struct {
 	context.Context
 	cancel      context.CancelFunc
 	stoppedChan chan struct{}
 	client      *Client
-	handler     RecvEventHandler
+	handler     RecvDataHandler
 }
 
-func (w *_EventWatcher) Stop() <-chan struct{} {
+func (w *_DataWatcher) Stop() <-chan struct{} {
 	w.cancel()
 	return w.stoppedChan
 }
 
-func (w *_EventWatcher) mainLoop() {
+func (w *_DataWatcher) mainLoop() {
 	defer func() {
 		w.cancel()
 		w.client.wg.Done()
@@ -52,8 +52,8 @@ func (w *_EventWatcher) mainLoop() {
 	case <-w.client.Done():
 	}
 
-	w.client.eventWatchers.AutoLock(func(watchers *[]*_EventWatcher) {
-		*watchers = pie.DropWhile(*watchers, func(other *_EventWatcher) bool {
+	w.client.dataWatchers.AutoLock(func(watchers *[]*_DataWatcher) {
+		*watchers = pie.DropWhile(*watchers, func(other *_DataWatcher) bool {
 			return other == w
 		})
 	})
