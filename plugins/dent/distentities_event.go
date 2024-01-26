fcab3186777952d9456191bfc340a6ac384c7b3a
pangdogs <pangdogs@gmail.com>
2024-01-26 14:00:25 +0800

    更新代码

---
+++ plugins/dent/distentities_event.go
@@ -0,0 +1,17 @@
+//go:generate go run git.golaxy.org/core/event/eventcode --decl_file=$GOFILE gen_event --package=$GOPACKAGE --default_export=false
+//go:generate go run git.golaxy.org/core/event/eventcode --decl_file=$GOFILE gen_eventtab --package=$GOPACKAGE --name=DistEntitiesEventTab
+package dent
+
+import (
+	"git.golaxy.org/core/ec"
+)
+
+// EventDistEntityOnline 事件：分布式实体上线
+type EventDistEntityOnline interface {
+	OnDistEntityOnline(entity ec.Entity)
+}
+
+// EventDistEntityOffline 事件：分布式实体下线
+type EventDistEntityOffline interface {
+	OnDistEntityOffline(entity ec.Entity)
+}
