4089324e3004414fe7f9d089ab9102a8d282cedb
pangdogs <pangdogs@gmail.com>
2024-01-28 15:11:45 +0800

    修改代码

--- oc/comp_async.go
+++ oc/comp_async.go
@@ -1,1 +1,64 @@
 package oc
+
+import (
+	"context"
+	"errors"
+	"git.golaxy.org/core"
+	"git.golaxy.org/core/ec"
+	"git.golaxy.org/core/runtime"
+	"git.golaxy.org/core/util/generic"
+	"time"
+)
+
+var (
+	ErrComponentIsDead = errors.New("async/await: component is dead")
+)
+
+// Async 异步执行代码，有返回值
+func (c *ComponentBehavior) Async(fun generic.FuncVar0[any, runtime.Ret], va ...any) runtime.AsyncRet {
+	return core.Async(c.GetRtCtx(), func(_ runtime.Context, a ...any) runtime.Ret {
+		if c.GetState() > ec.ComponentState_Living {
+			return runtime.MakeRet(nil, ErrComponentIsDead)
+		}
+		return fun.Exec(a...)
+	}, va...)
+}
+
+// AsyncVoid 异步执行代码，无返回值
+func (c *ComponentBehavior) AsyncVoid(fun generic.ActionVar0[any], va ...any) runtime.AsyncRet {
+	return core.AsyncVoid(c.GetRtCtx(), func(_ runtime.Context, a ...any) {
+		if c.GetState() > ec.ComponentState_Living {
+			return
+		}
+		fun.Exec(a...)
+	}, va...)
+}
+
+// Go 使用新线程执行代码，有返回值（注意线程安全）
+func (c *ComponentBehavior) Go(fun generic.FuncVar0[any, runtime.Ret], va ...any) runtime.AsyncRet {
+	return core.Go(c.GetRtCtx(), func(_ context.Context, a ...any) runtime.Ret {
+		return fun.Exec(a...)
+	}, va...)
+}
+
+// GoVoid 使用新线程执行代码，无返回值（注意线程安全）
+func (c *ComponentBehavior) GoVoid(fun generic.ActionVar0[any], va ...any) runtime.AsyncRet {
+	return core.GoVoid(c.GetRtCtx(), func(_ context.Context, a ...any) {
+		fun.Exec(a...)
+	}, va...)
+}
+
+// TimeAfter 定时器，指定时长
+func (c *ComponentBehavior) TimeAfter(dur time.Duration) runtime.AsyncRet {
+	return core.TimeAfter(c.GetRtCtx(), dur)
+}
+
+// TimeAt 定时器，指定时间点
+func (c *ComponentBehavior) TimeAt(at time.Time) runtime.AsyncRet {
+	return core.TimeAt(c.GetRtCtx(), at)
+}
+
+// TimeTick 心跳器
+func (c *ComponentBehavior) TimeTick(dur time.Duration) runtime.AsyncRet {
+	return core.TimeTick(c.GetRtCtx(), dur)
+}
--- oc/comp_await.go
+++ oc/comp_await.go
@@ -1,1 +1,62 @@
 package oc
+
+import (
+	"git.golaxy.org/core"
+	"git.golaxy.org/core/ec"
+	"git.golaxy.org/core/runtime"
+	"git.golaxy.org/core/util/generic"
+)
+
+// AwaitDirector 异步等待分发器
+type AwaitDirector struct {
+	cb       *ComponentBehavior
+	director core.AwaitDirector
+}
+
+// Any 异步等待任意一个结果返回
+func (ad AwaitDirector) Any(fun generic.ActionVar1[runtime.Ret, any], va ...any) {
+	ad.director.Any(runtime.Current(ad.cb), func(_ runtime.Context, ret runtime.Ret, a ...any) {
+		if ad.cb.GetState() > ec.ComponentState_Living {
+			return
+		}
+		fun.Exec(ret, a...)
+	}, va...)
+}
+
+// AnyOK 异步等待任意一个结果成功返回
+func (ad AwaitDirector) AnyOK(fun generic.ActionVar1[runtime.Ret, any], va ...any) {
+	ad.director.AnyOK(runtime.Current(ad.cb), func(_ runtime.Context, ret runtime.Ret, a ...any) {
+		if ad.cb.GetState() > ec.ComponentState_Living {
+			return
+		}
+		fun.Exec(ret, a...)
+	}, va...)
+}
+
+// All 异步等待所有结果返回
+func (ad AwaitDirector) All(fun generic.ActionVar1[[]runtime.Ret, any], va ...any) {
+	ad.director.All(runtime.Current(ad.cb), func(_ runtime.Context, rets []runtime.Ret, a ...any) {
+		if ad.cb.GetState() > ec.ComponentState_Living {
+			return
+		}
+		fun.Exec(rets, a...)
+	}, va...)
+}
+
+// Pipe 异步等待管道返回
+func (ad AwaitDirector) Pipe(fun generic.ActionVar1[runtime.Ret, any], va ...any) {
+	ad.director.Pipe(runtime.Current(ad.cb), func(_ runtime.Context, ret runtime.Ret, a ...any) {
+		if ad.cb.GetState() > ec.ComponentState_Living {
+			return
+		}
+		fun.Exec(ret, a...)
+	}, va...)
+}
+
+// Await 异步等待结果返回
+func (c *ComponentBehavior) Await(asyncRet ...runtime.AsyncRet) AwaitDirector {
+	return AwaitDirector{
+		cb:       c,
+		director: core.Await(c.GetRtCtx(), asyncRet...),
+	}
+}
