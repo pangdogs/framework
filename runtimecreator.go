9f4b3d6f0151ba7924918e95070918cdae454374
pangdogs <pangdogs@gmail.com>
2024-02-20 20:18:17 +0800

    修改代码

--- runtimecreator.go
+++ runtimecreator.go
@@ -6,6 +6,7 @@ import (
 	"git.golaxy.org/core/service"
 )
 
+// CreateRuntime 创建运行时
 func CreateRuntime(ctx service.Context) RuntimeCreator {
 	if ctx == nil {
 		panic(fmt.Errorf("%w: ctx is nil", core.ErrArgs))
@@ -24,12 +25,14 @@ func CreateRuntime(ctx service.Context) RuntimeCreator {
 	}
 }
 
+// RuntimeCreator 运行时构建器
 type RuntimeCreator struct {
 	servCtx  service.Context
 	runtime  _IRuntime
 	settings _RuntimeSettings
 }
 
+// Setup 安装运行时
 func (c RuntimeCreator) Setup(rt any) RuntimeCreator {
 	if c.servCtx == nil {
 		panic("setting servCtx is nil")
@@ -49,32 +52,38 @@ func (c RuntimeCreator) Setup(rt any) RuntimeCreator {
 	return c
 }
 
+// Name 名称
 func (c RuntimeCreator) Name(name string) RuntimeCreator {
 	c.settings.Name = name
 	return c
 }
 
+// PanicHandling panic时的处理方式
 func (c RuntimeCreator) PanicHandling(autoRecover bool, reportError chan error) RuntimeCreator {
 	c.settings.AutoRecover = autoRecover
 	c.settings.ReportError = reportError
 	return c
 }
 
+// FPS 帧率
 func (c RuntimeCreator) FPS(fps float32) RuntimeCreator {
 	c.settings.FPS = fps
 	return c
 }
 
+// AutoRun 自动开始运行
 func (c RuntimeCreator) AutoRun(auto bool) RuntimeCreator {
 	c.settings.AutoRun = auto
 	return c
 }
 
+// ProcessQueueCapacity 任务处理流水线大小
 func (c RuntimeCreator) ProcessQueueCapacity(cap int) RuntimeCreator {
 	c.settings.ProcessQueueCapacity = cap
 	return c
 }
 
+// Spawn 创建运行时
 func (c RuntimeCreator) Spawn() core.Runtime {
 	if c.servCtx == nil {
 		panic("setting servCtx is nil")
