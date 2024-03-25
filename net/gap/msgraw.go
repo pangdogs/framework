6cb7bd2bdaa536f1e148ee4cb603b24d9ead559e
pangdogs <pangdogs@gmail.com>
2024-03-26 00:19:58 +0800

    修改代码

--- net/gap/msgraw.go
+++ net/gap/msgraw.go
@@ -1,6 +1,8 @@
 package gap
 
-import "git.golaxy.org/framework/util/binaryutil"
+import (
+	"io"
+)
 
 // MsgRaw 原始消息
 type MsgRaw struct {
@@ -10,29 +12,21 @@ type MsgRaw struct {
 
 // Read implements io.Reader
 func (m MsgRaw) Read(p []byte) (int, error) {
-	bs := binaryutil.NewBigEndianStream(p)
-	if err := bs.WriteBytes(m.Data); err != nil {
-		return bs.BytesWritten(), err
+	if len(p) < len(m.Data) {
+		return 0, io.ErrShortWrite
 	}
-	return bs.BytesWritten(), nil
+	return copy(p, m.Data), nil
 }
 
 // Write implements io.Writer
 func (m *MsgRaw) Write(p []byte) (int, error) {
-	bs := binaryutil.NewBigEndianStream(p)
-	var err error
-
-	m.Data, err = bs.ReadBytesRef()
-	if err != nil {
-		return bs.BytesRead(), err
-	}
-
-	return bs.BytesRead(), nil
+	m.Data = p
+	return len(p), nil
 }
 
 // Size 大小
 func (m MsgRaw) Size() int {
-	return binaryutil.SizeofUint32() + binaryutil.SizeofBytes(m.Data)
+	return len(m.Data)
 }
 
 // MsgId 消息Id
