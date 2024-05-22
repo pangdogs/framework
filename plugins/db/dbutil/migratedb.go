0dd5d3bf577eb5ff28392b36386fca2bc5ab17e0
pangdogs <pangdogs@gmail.com>
2024-05-22 13:16:05 +0800

    修改代码

--- plugins/db/dbutil/migratedb.go
+++ plugins/db/dbutil/migratedb.go
@@ -1,5 +1,28 @@
 package dbutil
 
-type IMigrateDB interface {
-	MigrateDB() error
+import (
+	"github.com/redis/go-redis/v9"
+	"go.mongodb.org/mongo-driver/mongo"
+	"gorm.io/gorm"
+)
+
+type DB interface {
+	*gorm.DB | *mongo.Client | *redis.Client
+}
+
+type IMigrateDB[T DB] interface {
+	MigrateDB(db T) error
+}
+
+func MigrateDB[T DB](db T, services ...any) error {
+	for _, service := range services {
+		migrateDB, ok := service.(IMigrateDB[T])
+		if !ok {
+			continue
+		}
+		if err := migrateDB.MigrateDB(db); err != nil {
+			return err
+		}
+	}
+	return nil
 }
