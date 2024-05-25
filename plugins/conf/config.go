ed1fcc8b312d5dfbe7fb3d2d4e145ed9b7caddfc
pangdogs <pangdogs@gmail.com>
2024-05-25 10:47:47 +0800

    修改代码

--- plugins/conf/config.go
+++ plugins/conf/config.go
@@ -5,9 +5,11 @@ import (
 	"git.golaxy.org/core/service"
 	"git.golaxy.org/core/util/option"
 	"git.golaxy.org/framework/plugins/log"
+	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	_ "github.com/spf13/viper/remote"
+	"time"
 )
 
 // IConfig 配置接口
@@ -24,8 +26,8 @@ func newConfig(settings ...option.Setting[ConfigOptions]) IConfig {
 
 type _Config struct {
 	options ConfigOptions
-	IVisitConf
-	whole IVisitConf
+	*_VisitConf
+	whole *_VisitConf
 }
 
 // InitSP 初始化服务插件
@@ -56,6 +58,8 @@ func (c *_Config) InitSP(ctx service.Context) {
 		if err := vp.ReadInConfig(); err != nil {
 			log.Panicf(ctx, "read local config %q failed, %s", c.options.LocalPath, err)
 		}
+
+		log.Infof(ctx, "load local config %q config ok", c.options.LocalPath)
 	}
 
 	if remote {
@@ -65,29 +69,55 @@ func (c *_Config) InitSP(ctx service.Context) {
 		if err := vp.ReadRemoteConfig(); err != nil {
 			log.Panicf(ctx, "read remote config [%q, %q, %q] failed, %s", c.options.RemoteProvider, c.options.RemoteEndpoint, c.options.RemotePath, err)
 		}
-	}
 
-	if c.options.AutoUpdate {
-		if local {
-			vp.WatchConfig()
-		}
-		if remote {
-			if err := vp.WatchRemoteConfigOnChannel(); err != nil {
-				log.Panicf(ctx, "watch remote config [%q, %q, %q] changes failed, %s", c.options.RemoteProvider, c.options.RemoteEndpoint, c.options.RemotePath, err)
-			}
-		}
+		log.Infof(ctx, "load remote config [%q, %q, %q] ok", c.options.RemoteProvider, c.options.RemoteEndpoint, c.options.RemotePath)
 	}
 
 	subVp := vp.Sub(ctx.GetName())
 	if subVp == nil {
 		subVp = viper.New()
 	}
-
-	c.IVisitConf = &_VisitConf{
+	c._VisitConf = &_VisitConf{
 		Viper: subVp,
 	}
 	c.whole = &_VisitConf{
 		Viper: vp,
+	}
+
+	if c.options.AutoUpdate {
+		if local {
+			vp.OnConfigChange(func(in fsnotify.Event) {
+				subVp := vp.Sub(ctx.GetName())
+				if subVp == nil {
+					subVp = viper.New()
+				}
+				c._VisitConf.Viper = subVp
+
+				log.Infof(ctx, "reload local config %q ok", c.options.LocalPath)
+			})
+			vp.WatchConfig()
+		}
+		if remote {
+			go func() {
+				for {
+					time.Sleep(time.Second * 3)
+
+					err := vp.WatchRemoteConfig()
+					if err != nil {
+						log.Errorf(ctx, "watch remote config [%q, %q, %q] changes failed, %s", c.options.RemoteProvider, c.options.RemoteEndpoint, c.options.RemotePath, err)
+						continue
+					}
+
+					subVp := vp.Sub(ctx.GetName())
+					if subVp == nil {
+						subVp = viper.New()
+					}
+					c._VisitConf.Viper = subVp
+
+					log.Infof(ctx, "reload remote config [%q, %q, %q] ok", c.options.RemoteProvider, c.options.RemoteEndpoint, c.options.RemotePath)
+				}
+			}()
+		}
 	}
 }
 
