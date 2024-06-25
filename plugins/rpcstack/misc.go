3446122d2c19d1c0994af417a5604ac727e10443
pangdogs <pangdogs@gmail.com>
2024-06-25 08:57:56 +0800

    修改代码

--- plugins/rpcstack/misc.go
+++ plugins/rpcstack/misc.go
@@ -12,30 +12,4 @@ type (
 
 var EmptyCallChain = CallChain{}
 
-type Variables generic.UnorderedSliceMap[string, any]
-
-func (m *Variables) Set(k string, v any) {
-	(*generic.UnorderedSliceMap[string, any])(m).Add(k, v)
-}
-
-func (m Variables) Get(k string) any {
-	v, ok := (generic.UnorderedSliceMap[string, any])(m).Get(k)
-	if !ok {
-		return nil
-	}
-	return v
-}
-
-func (m Variables) Range(fun generic.Func2[string, any, bool]) {
-	for _, kv := range (generic.UnorderedSliceMap[string, any])(m) {
-		if !fun.Exec(kv.K, kv.V) {
-			return
-		}
-	}
-}
-
-func (m Variables) Each(fun generic.Action2[string, any]) {
-	for _, kv := range (generic.UnorderedSliceMap[string, any])(m) {
-		fun.Exec(kv.K, kv.V)
-	}
-}
+type Variables = generic.UnorderedSliceMap[string, any]
