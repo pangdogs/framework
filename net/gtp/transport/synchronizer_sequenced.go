dc2b7c69b37ee2157efce1b11fd09dbe641771e8
pangdogs <pangdogs@gmail.com>
2025-05-06 12:11:20 +0800

    修改gtp协议同步器使用环形buff

--- net/gtp/transport/synchronizer_sequenced.go
+++ net/gtp/transport/synchronizer_sequenced.go
@@ -36,22 +36,103 @@ func NewSequencedSynchronizer(sendSeq, recvSeq uint32, cap int) ISynchronizer {
 	return s
 }
 
-// _SequencedFrame 时序帧
-type _SequencedFrame struct {
+const (
+	queueMinSize = 16
+)
+
+// _Frame 帧
+type _Frame struct {
 	seq    uint32 // 序号
 	offset int    // 帧数据偏移位置
 	data   []byte // 帧数据
 }
 
+// _Queue 环形队列
+type _Queue struct {
+	buf               []_Frame
+	head, tail, count int
+}
+
+func newQueue() *_Queue {
+	return &_Queue{
+		buf: make([]_Frame, queueMinSize),
+	}
+}
+
+func (q *_Queue) Length() int {
+	return q.count
+}
+
+func (q *_Queue) Push(elem _Frame) {
+	if q.count == len(q.buf) {
+		q.resize()
+	}
+
+	q.buf[q.tail] = elem
+
+	q.tail = (q.tail + 1) & (len(q.buf) - 1)
+	q.count++
+}
+
+func (q *_Queue) Peek() *_Frame {
+	if q.count <= 0 {
+		panic(fmt.Errorf("%w: queue: Peek() called on empty queue", ErrSynchronizer))
+	}
+	return &q.buf[q.head]
+}
+
+func (q *_Queue) Index(i int) *_Frame {
+	if i < 0 {
+		i += q.count
+	}
+	if i < 0 || i >= q.count {
+		panic(fmt.Errorf("%w: queue: Index() called with index out of range", ErrSynchronizer))
+	}
+	return &q.buf[(q.head+i)&(len(q.buf)-1)]
+}
+
+func (q *_Queue) Pop() _Frame {
+	if q.count <= 0 {
+		panic(fmt.Errorf("%w: queue: Pop() called on empty queue", ErrSynchronizer))
+	}
+
+	elem := q.buf[q.head]
+	q.buf[q.head] = _Frame{}
+
+	q.head = (q.head + 1) & (len(q.buf) - 1)
+	q.count--
+
+	if len(q.buf) > queueMinSize && (q.count<<2) == len(q.buf) {
+		q.resize()
+	}
+
+	return elem
+}
+
+func (q *_Queue) resize() {
+	newBuf := make([]_Frame, q.count<<1)
+
+	if q.tail > q.head {
+		copy(newBuf, q.buf[q.head:q.tail])
+	} else {
+		n := copy(newBuf, q.buf[q.head:])
+		copy(newBuf[n:], q.buf[:q.tail])
+	}
+
+	q.head = 0
+	q.tail = q.count
+	q.buf = newBuf
+}
+
 // SequencedSynchronizer 有时序同步器，支持缓存已发送的消息，在断连重连时同步时序并补发消息
 type SequencedSynchronizer struct {
-	sendSeq uint32            // 发送消息序号
-	recvSeq uint32            // 接收消息序号
-	ackSeq  uint32            // 当前ack序号
-	cap     int               // 缓存区容量（字节），缓存区满时将会触发清理操作，此时断线重连有可能会失败
-	cached  int               // 已缓存大小（字节）
-	queue   []_SequencedFrame // 窗口队列
-	sent    int               // 已发送位置
+	sendSeq uint32  // 发送消息序号
+	recvSeq uint32  // 接收消息序号
+	ackSeq  uint32  // 当前ack序号
+	cap     int     // 缓存区容量（字节），缓存区满时将会触发清理操作，此时断线重连有可能会失败
+	cached  int     // 已缓存大小（字节）
+	queue   *_Queue // 窗口队列
+	sent    int     // 已发送位置
 }
 
 func (s *SequencedSynchronizer) init(sendSeq, recvSeq uint32, cap int) {
@@ -60,7 +141,7 @@ func (s *SequencedSynchronizer) init(sendSeq, recvSeq uint32, cap int) {
 	s.ackSeq = sendSeq - 1
 	s.cap = cap
 	s.cached = 0
-	s.queue = nil
+	s.queue = newQueue()
 	s.sent = 0
 }
 
@@ -94,7 +175,8 @@ func (s *SequencedSynchronizer) Write(p []byte) (n int, err error) {
 	}
 
 	// 写入帧队列并自增序号
-	s.append(head.Seq, data)
+	s.queue.Push(_Frame{seq: s.sendSeq, data: data})
+	s.cached += len(data)
 	s.sendSeq++
 
 	return len(data), nil
@@ -109,8 +191,8 @@ func (s *SequencedSynchronizer) WriteTo(w io.Writer) (int64, error) {
 	var wn int64
 
 	// 读取帧队列，向输出流写入消息
-	for i := s.sent; i < len(s.queue); i++ {
-		frame := &s.queue[i]
+	for i := s.sent; i < s.queue.Length(); i++ {
+		frame := s.queue.Index(i)
 
 		if frame.offset < len(frame.data) {
 			n, err := w.Write(frame.data[frame.offset:])
@@ -145,13 +227,13 @@ func (s *SequencedSynchronizer) Validate(msgHead gtp.MsgHead, msgBuf []byte) err
 // Synchronize 同步对端时序，对齐缓存序号
 func (s *SequencedSynchronizer) Synchronize(remoteRecvSeq uint32) error {
 	// 从时序帧中查询对端序号
-	for i := len(s.queue) - 1; i >= 0; i-- {
-		frame := &s.queue[i]
+	for i := s.queue.Length() - 1; i >= 0; i-- {
+		frame := s.queue.Index(i)
 
 		d := int32(frame.seq - remoteRecvSeq)
 		if d <= 0 {
-			for j := i; j < len(s.queue); j++ {
-				s.queue[j].offset = 0
+			for j := i; j < s.queue.Length(); j++ {
+				s.queue.Index(j).offset = 0
 			}
 
 			s.sent = i
@@ -209,10 +291,10 @@ func (s *SequencedSynchronizer) Clean() {
 	s.ackSeq = 0
 	s.cap = 0
 	s.cached = 0
-	for i := range s.queue {
-		binaryutil.BytesPool.Put(s.queue[i].data)
+	for i := 0; i < s.queue.Length(); i++ {
+		binaryutil.BytesPool.Put(s.queue.Index(i).data)
 	}
-	s.queue = nil
+	s.queue = newQueue()
 	s.sent = 0
 }
 
@@ -224,26 +306,19 @@ func (s *SequencedSynchronizer) getRemoteAck() uint32 {
 	return atomic.LoadUint32(&s.ackSeq)
 }
 
-func (s *SequencedSynchronizer) append(seq uint32, data []byte) {
-	// 写入帧队列
-	s.queue = append(s.queue, _SequencedFrame{seq: seq, data: data})
-	s.cached += len(data)
-}
-
 func (s *SequencedSynchronizer) ack(seq uint32) {
 	cached := s.cached
 
-	for i := range s.queue {
-		frame := &s.queue[i]
+	for i := 0; i < s.queue.Length(); i++ {
+		frame := s.queue.Index(i)
 
 		cached -= len(frame.data)
 
 		if frame.seq == seq {
 			for j := 0; j <= i; j++ {
-				binaryutil.BytesPool.Put(s.queue[j].data)
+				binaryutil.BytesPool.Put(s.queue.Pop().data)
 			}
 
-			s.queue = append(s.queue[:0], s.queue[i+1:]...)
 			s.sent = 0
 
 			s.cached = cached
@@ -251,7 +326,7 @@ func (s *SequencedSynchronizer) ack(seq uint32) {
 				exception.Panicf("%w: sequenced buffer cached less 0 invalid", ErrSynchronizer)
 			}
 
-			break
+			return
 		}
 	}
 }
@@ -260,17 +335,16 @@ func (s *SequencedSynchronizer) reduce(size int) {
 	cached := s.cached
 
 	for i := 0; i < s.sent; i++ {
-		frame := &s.queue[i]
+		frame := s.queue.Index(i)
 
 		cached -= len(frame.data)
 
 		size -= len(frame.data)
 		if size <= 0 {
 			for j := 0; j <= i; j++ {
-				binaryutil.BytesPool.Put(s.queue[j].data)
+				binaryutil.BytesPool.Put(s.queue.Pop().data)
 			}
 
-			s.queue = append(s.queue[:0], s.queue[i+1:]...)
 			s.sent = 0
 
 			s.cached = cached
@@ -278,7 +352,7 @@ func (s *SequencedSynchronizer) reduce(size int) {
 				exception.Panicf("%w: sequenced buffer cached less 0 invalid", ErrSynchronizer)
 			}
 
-			break
+			return
 		}
 	}
 }
