ee55b384ea039ff0cffe3e3d354718ce26d906db
pangdogs <pangdogs@gmail.com>
2026-04-07 09:29:50 +0800

    整理代码

---
+++ net/doc.go
@@ -0,0 +1,28 @@
+/*
+ * This file is part of Golaxy Distributed Service Development Framework.
+ *
+ * Golaxy Distributed Service Development Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Golaxy Distributed Service Development Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Golaxy Distributed Service Development Framework. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2024 pangdogs.
+ */
+
+// Package net 汇总框架中的网络通信相关能力。
+//
+// 这个目录下的子包主要包括：
+//   - gap：应用层消息协议
+//   - gtp：传输层长连接协议
+//   - netpath：协议路径和路由字符串处理
+//
+// 根包本身不提供具体实现，主要用于承载网络子系统的总览文档。
+package net
---
+++ utils/doc.go
@@ -0,0 +1,27 @@
+/*
+ * This file is part of Golaxy Distributed Service Development Framework.
+ *
+ * Golaxy Distributed Service Development Framework is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Golaxy Distributed Service Development Framework is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Golaxy Distributed Service Development Framework. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2024 pangdogs.
+ */
+
+// Package utils 汇总框架中的通用基础工具。
+//
+// 当前主要子包包括：
+//   - binaryutil：二进制读写、字节流和字节池工具
+//   - concurrent：Future 控制、监听器集合等并发辅助组件
+//
+// 根包本身不提供具体实现，主要用于承载工具层的总览文档。
+package utils
